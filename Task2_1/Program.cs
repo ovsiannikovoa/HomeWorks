ed3ef00cce60fbf05689222ae4ff212e72ef3c61
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-17 22:52:10 +0300

    Сделал задачу 2.1

---
+++ Task2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int t = c;
+        c = b;
+        b = a;
+        a = t;
+
+        Console.WriteLine("a={0}, b={1}, c={2}", a, b, c);
+        Console.ReadKey();
+    }
+}
