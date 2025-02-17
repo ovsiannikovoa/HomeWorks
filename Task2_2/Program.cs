649aee47dbbf313f9587a2a18b995b5b01a50e75
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-17 23:07:51 +0300

    Сделал задание 2.2

---
+++ Task2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deg = Convert.ToInt32(Console.ReadLine());
+            int min = Convert.ToInt32(Console.ReadLine());
+            int sec = Convert.ToInt32(Console.ReadLine());
+
+            int degInSec = deg * 60 * 60 + min * 60 + sec;
+            double degInRad = (degInSec * Math.PI / 180 / 60 / 60);
+
+            Console.WriteLine(degInRad);
+            Console.ReadKey();
+        }
+    }
+}
