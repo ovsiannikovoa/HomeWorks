c39b6157b2edfb2b1edfa89a5e510ac828da96dd
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-18 19:34:02 +0300

    Task2_5 is completed

---
+++ Task2_5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = Convert.ToDouble (Console.ReadLine());
+            int lengthTotal = (int)Math.Ceiling(length);
+            Console.WriteLine (lengthTotal);
+            Console.ReadKey();
+        }
+    }
+}
