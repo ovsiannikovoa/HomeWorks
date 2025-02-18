1c27c16a83187e7334cac38b740eed6c3993c09c
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-18 22:47:11 +0300

    Task2_7 is completed

---
+++ Task2_7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            firstNum = firstNum + secondNum;
+            secondNum = firstNum - secondNum;
+            firstNum = firstNum - secondNum;    
+
+            Console.WriteLine("{0},{1}", firstNum, secondNum);
+            Console.ReadKey();
+        }
+    }
+}
