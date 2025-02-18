085fdc4928cbdb58557250283bf9a05a80844abb
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-18 22:31:28 +0300

    Task2_6 is completed

---
+++ Task2_6/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double len = Convert.ToDouble(Console.ReadLine());
+            double roundedLen = Math.Round(len * 2) / 2;
+
+            Console.WriteLine(roundedLen);
+            Console.ReadKey();
+        }
+    }
+}
