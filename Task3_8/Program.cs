532629314e793fc731e3ea8eff46c5f681490110
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-25 23:08:21 +0300

    fix

---
+++ Task3_8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 10 && num >= -10)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
