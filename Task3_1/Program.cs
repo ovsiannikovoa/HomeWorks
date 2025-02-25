ad41070d297546760143ac538f7167a64f262a21
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-25 12:20:44 +0300

    fix

--- Task3_1/Program.cs
+++ Task3_1/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a == b)
+            {
+                Console.WriteLine('=');
+            }
+            else
+            {
+                if (a > b)
+                {
+                    Console.WriteLine('>');
+                }
+                else
+                {
+                    Console.WriteLine('<');
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
