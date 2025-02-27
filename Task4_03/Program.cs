e729edf08eabd51e0c80abfcf9d11865ea62d128
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-02-27 22:34:06 +0300

    4_03

---
+++ Task4_03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task4_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a < 0 || b < 0)
+            {
+                Console.WriteLine("Введенные значения не соответствуют требованиям");
+            }
+
+            int result = a;
+            for (int i = 2; i <= b; i++)
+            {
+                result *= a;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
