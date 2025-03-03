f47a041c50ff36ae546a5d33509af0d74ddf5b3d
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-03-03 18:30:52 +0300

    4_07

---
+++ Task4_07/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task4_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            if (num == 0)
+            {
+                count = 1;
+            }
+            else
+            {
+                num = (num < 0) ? -num : num;
+
+                while (num > 0)
+                {
+                    num /= 10;
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
