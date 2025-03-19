1f0af9de444b0813d7d885492afdc626d6a590c2
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-03-19 21:47:33 +0300

    Task06_03 unfinished. Task06_04

---
+++ Task06_04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Task06_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            string age = Console.ReadLine();
+
+            Console.Write("Введите город: ");
+            string city = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Имя: ");
+            sb.Append(name);
+            sb.Append(". Возраст: ");
+            sb.Append(age);
+            sb.Append(". Город: ");
+            sb.Append(city);
+            Console.Write(sb.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
