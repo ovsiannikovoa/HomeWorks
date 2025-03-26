0f794666b2b442137347bf4a7294244b53d24dd3
ovsiannikovoa <ovsiannikovoa@outlook.com>
2025-03-26 21:58:04 +0300

    07_04

---
+++ Task07_04/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task07_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+            int[] array = new int[n];
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+            }
+
+            for (int i = 0;i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            FindMax(array);
+
+            Console.ReadKey();
+        }
+
+        static void FindMax(params int[] nums)
+        {
+            int maxNumber = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > maxNumber)
+                {
+                    maxNumber = nums[i];
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+        }
+    }
+}
