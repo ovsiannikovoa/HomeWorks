da20151c78b5f54445b7f42bdc73d3e70d370c77
ovsiannikovoa <ovsiannikovoa@pik.ru>
2025-04-21 23:58:15 +0300

    11_01

--- Task11_01/Program.cs
+++ Task11_01/Program.cs
@@ -1,11 +1,10 @@
-﻿namespace Task1_1
+﻿namespace Task11_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.ReadKey();
         }
     }
 }
