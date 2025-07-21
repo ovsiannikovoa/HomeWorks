7ad3f2ccfa5a63674c5fa51c361c02ab948025de
ovsiannikovoa <ovsiannikovoa@pik.ru>
2025-07-21 19:22:26 +0300

    02_04_01

---
+++ HW02_04_01/ToggleButton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HW02_04_01
+{
+    public class ToggleButton : Button
+    {
+        public static readonly DependencyProperty IsToggledProperty =
+            DependencyProperty.Register(
+                nameof(IsToggled),
+                typeof(bool),
+                typeof(ToggleButton),
+                new FrameworkPropertyMetadata(
+                    false,
+                    FrameworkPropertyMetadataOptions.None,
+                    OnIsToggledChanged));
+
+        public bool IsToggled
+        {
+            get => (bool)GetValue(IsToggledProperty);
+            set => SetValue(IsToggledProperty, value);
+        }
+
+        private static void OnIsToggledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var button = (ToggleButton)d;
+            button.UpdateAppearance();
+        }
+
+        private void UpdateAppearance()
+        {
+            Content = IsToggled ? "ON" : "OFF";
+            Background = IsToggled
+                ? Brushes.Green
+                : Brushes.Red;
+        }
+
+        public ToggleButton()
+        {
+            // Инициализация внешнего вида
+            UpdateAppearance();
+
+            // Обработка клика
+            Click += (sender, e) => IsToggled = !IsToggled;
+        }
+    }
+}
