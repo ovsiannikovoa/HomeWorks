34a927969859b3fab6ba1f4df3e32517290c8109
ovsiannikovoa <ovsiannikovoa@pik.ru>
2025-07-22 19:01:14 +0300

    02_05_01

---
+++ Task02_05_01/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Task02_05_01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
